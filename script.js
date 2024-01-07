f138733486651315e3620f42277032ec96367bae
Trần Phạm Duy Minh <129474986+tranphamduyminh-dev@users.noreply.github.com>
2024-01-07 15:44:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Các thông tin ủng hộ
+const accountNumber = "Tran Pham Duy Minh | MBBank | 0859992120";
+
+// Đặt nội dung vào trang HTML
+document.getElementById("accountNumber").textContent = accountNumber;
