94acb6418dd52229f4b7db4f149edf1f53fc6d5a
Trần Phạm Duy Minh <129474986+tranphamduyminh-dev@users.noreply.github.com>
2024-01-07 16:04:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,2 @@
-// Các thông tin ủng hộ
 const accountNumber = "Tran Pham Duy Minh | MBBank | 0859992120";
-
-// Đặt nội dung vào trang HTML
 document.getElementById("accountNumber").textContent = accountNumber;
